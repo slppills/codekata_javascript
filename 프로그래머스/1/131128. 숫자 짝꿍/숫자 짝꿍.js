1ba873ec840532afde1a81ae6c0ed6243287a73b
slppills <sunchaehoon2@naver.com>
2024-07-05 19:30:30 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.js
@@ -1,7 +1,6 @@
 function solution(X, Y) {
     let answer = '';
     
-    // 각 문자의 빈도수를 계산합니다.
     const countX = Array(10).fill(0);
     const countY = Array(10).fill(0);
     
@@ -13,7 +12,6 @@ function solution(X, Y) {
         countY[char]++;
     }
     
-    // 각 문자의 최소 빈도수를 찾아 공통된 문자를 추가합니다.
     for (let i = 9; i >= 0; i--) {
         const commonCount = Math.min(countX[i], countY[i]);
         if (commonCount > 0) {
