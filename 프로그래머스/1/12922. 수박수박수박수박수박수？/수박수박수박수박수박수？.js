18b1eca48cb75bd37605a5d1ec1f48fb840ed08e
slppills <sunchaehoon2@naver.com>
2024-08-01 20:53:09 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.53 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,11 +1,7 @@
 function solution(n) {
-    let answer = '';
-    for (let i=0; i<n; i++) {
-        if (i % 2 === 0) {
-            answer += '수';
-        } else {
-            answer += '박';
-        }
+    let answer = "";
+    for (let i=1; i<=n; i++) {
+        i % 2 !== 0 ? answer += "수" : answer += "박";
     }
     return answer;
 }
