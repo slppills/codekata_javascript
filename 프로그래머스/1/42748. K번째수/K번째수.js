8416827ccbbc2680b97df6ee35e62f149607f091
slppills <sunchaehoon2@naver.com>
2024-08-05 21:09:31 +0900

    [level 1] Title: K번째수, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,8 +1,7 @@
 function solution(array, commands) {
     let answer = [];
     for (let i=0; i<commands.length; i++) {
-        arr = array.slice(commands[i][0]-1, commands[i][1]);
-        arr.sort((a, b) => a-b);
+        const arr = array.slice(commands[i][0]-1, commands[i][1]).sort((a, b) => a-b);
         answer.push(arr[commands[i][2]-1]);
     }
     return answer;
