150696210d47166901b8b41e105f021adc83a52e
slppills <sunchaehoon2@naver.com>
2024-12-19 05:15:51 +0900

    [Silver II] Title: 스택 수열, Time: 228 ms, Memory: 33868 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.js
+++ 백준/Silver/1874. 스택 수열/스택 수열.js
@@ -5,22 +5,14 @@ const stack = [];
 const result = [];
 let order = 1;
 
-while (seq.length > 0 || stack.length > 0) {
-  if (stack.length === 0) {
-    stack.push(seq.shift());
-    result.push("+");
-  }
-  if (stack[stack.length-1] === Number(input[order])) {
+for (let i = 0; i < N; i++) {
+  stack.push(seq[i]);
+  result.push("+");
+
+  while (stack.length > 0 && stack[stack.length - 1] === Number(input[order])) {
     stack.pop();
     result.push("-");
     order++;
-  } else {
-    if (seq.length === 0) {
-      break;
-    } else {
-      stack.push(seq.shift());
-      result.push("+");
-    }
   }
 }
 
